cb4d618cdc0634a47e293662468e9697521d04a4
MD SARFARAZ <159709636+LearnWithNewton@users.noreply.github.com>
2024-08-20 11:50:50 +0000

    updated app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,109 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.title("AiDE Prompt Guide")
+st.markdown(
+    """
+# THE C.R.E.A.T.E FRAMEWORK
+
+If you want to write an effective 
+prompt, you need to brief ChatGPT 
+properly.
+Here’s a reminder of the different 
+elements you should consider for 
+your prompt.
+
+### **C**haracter
+
+This is the role you want ChatGPT to 
+play. It could be an expert, a celebrity, 
+an artist or an idiot. Make it clear with 
+an introductory statement.
+
+> Sample Prompt : “You are an expert chef with 20 years 
+experience and a Michelin star. You 
+can create amazing and surprising 
+dishes from the simplest of 
+ingredients.”
+
+
+### **R**equest
+
+This is the task you want ChatGPT to 
+do for you. You clearer you are, the 
+higher your chance of getting a great 
+response. Give more information if 
+you need to.
+
+> Sample Prompt : “I want you to generate an amazing 
+recipe just using the ingredients I 
+have in my fridge and cupboards. 
+Start by asking me what ingredients 
+I have available.”
+
+### **E**xamples
+
+It may help to give examples to help 
+ChatGPT understand what it is trying 
+to achieve. Lists of information can be 
+particularly good.
+
+> Sample Prompt : “Draw your inspiration from chefs 
+like Marco Pierre White, Gordon 
+Ramsey, Anne-Sophie Pic and 
+Yannick Alléno.”
+
+### **A**dditions
+
+You’ll often discover elements in the 
+output that aren’t quite what you 
+wanted. This is where you add some 
+extra statements to help shape the 
+results.
+
+> Sample Prompt : “List the measurements in imperial 
+units. Do not use all the ingredients - 
+only use what you need for the dish. 
+Do not suggest a recipe until I give 
+you my ingredients. Ask for the 
+ingredients and then wait for my 
+response before answering.”
+
+### **T**ype of output
+
+Tell ChatGPT exactly how to format 
+y o u r o u t p u t . Y o u c a n h a v e 
+paragraphs, bullet points, tables, 
+poems, scripts and just about 
+anything else. Make it clear how you 
+want the output to be formatted.
+
+> Sample Prompt : “Write your response as a recipe. 
+Give me a recipe title, a 100 word 
+paragraph describing the dish, a list 
+of ingredients and simple-to-follow 
+steps. Write it in plain English 
+without jargon.”
+
+### **E**xtras
+
+There are some powerful phrases you 
+can add to your prompt to make it 
+work better.
+
+> Sample Prompts :
+
+- “IGNORE EVERYTHING BEFORE THIS PROMPT.”
+- “ASK ME QUESTIONS BEFORE YOU ANSWER.”
+- “EXPLAIN YOUR THINKING.”
+- “ACT UNLIKE A TYPICAL AI.”
+- 
+
+### Prompt Samples
+
+> Summarize a passage, You are a highly experienced writer who writes concise and readable text without stop words, filler words or jargon. I want you to summarise the following text, highlighting the most important concepts. Deliver this as a short paragraph of 100 words. Then list the most important points as a bullet-point list. Finally, follow it with a one sentence summary. The text I want you to summarise is "[TEXT]"0
+
+> 
+
+"""
+    
 )
